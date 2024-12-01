db27e4c49d704ecfeb8ba980492c6c2ac26c0ddd
vishaldeep09 <09vishaldeep@gmail.com>
2024-12-01 23:09:57 +0530

    initial project setup and form component part completed

---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmployeeService } from '../services/employee.service';
+import { Router } from '@angular/router';
+import { DialogRef } from '@angular/cdk/dialog';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrls: ['./emp-add-edit.component.scss']
+})
+export class EmpAddEditComponent implements OnInit{
+
+  empForm!:FormGroup;
+
+  education:string[]=[
+    'Matric',
+    'Diploma',
+    'Intermediate',
+    'Graduate',
+    'Post Graduate'
+  ]
+
+  //inside DialogRef<mention current class name>, after submitting form it will that box
+  constructor(private _fb:FormBuilder,private _empService:EmployeeService,private _router:Router,
+    private _dialogRef:DialogRef<EmpAddEditComponent>){}
+
+  ngOnInit(): void {
+   this.empForm=this._fb.group({
+      firstName:'',
+      lastName:'',
+      email:'',
+      dob:'',
+      gender:'',
+      education:'',
+      company:'',
+      experience:'',
+      package:''
+   })
+   
+  }
+   
+  onFormSubmit(){
+  if(this.empForm.valid){
+    this._empService.addEmployee(this.empForm.value).subscribe({
+      next:(val)=>{
+        alert(`employee added successfully`);
+        this._dialogRef.close()
+      },
+      error:(err:any)=>{
+        console.error(err)
+      }
+    })
+  }}
+
+}
